6f0749b80928786dd433afc5ebd2eb83c6e06754
Baichuan7 <Cuhan237@gmail.com>
2024-09-11 17:29:22 +0800

    build is done 20240911

---
+++ build/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/liubaichuan/Work/slamdemo/src/backend.cpp" "src/CMakeFiles/myslam.dir/backend.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/backend.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/camera.cpp" "src/CMakeFiles/myslam.dir/camera.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/camera.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/config.cpp" "src/CMakeFiles/myslam.dir/config.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/config.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/dataset.cpp" "src/CMakeFiles/myslam.dir/dataset.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/dataset.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/feature.cpp" "src/CMakeFiles/myslam.dir/feature.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/feature.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/frame.cpp" "src/CMakeFiles/myslam.dir/frame.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/frame.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/frontend.cpp" "src/CMakeFiles/myslam.dir/frontend.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/frontend.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/map.cpp" "src/CMakeFiles/myslam.dir/map.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/map.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/mappoint.cpp" "src/CMakeFiles/myslam.dir/mappoint.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/mappoint.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/viewer.cpp" "src/CMakeFiles/myslam.dir/viewer.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/viewer.cpp.o.d"
+  "/home/liubaichuan/Work/slamdemo/src/visual_odometry.cpp" "src/CMakeFiles/myslam.dir/visual_odometry.cpp.o" "gcc" "src/CMakeFiles/myslam.dir/visual_odometry.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/liubaichuan/Work/slamdemo/src
+# Build directory: /home/liubaichuan/Work/slamdemo/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
